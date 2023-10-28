4e6c9beb87c96f90332dd2b69945d7a713cae0a1
vrutansh <vrutanshpatel01@gmail.com>
2023-10-28 18:14:20 +0530

    .

---
+++ Fi-Store/script.js
@@ -0,0 +1,12 @@
+vv = document.querySelectorAll(".imgcontainer");
+vv.forEach(function(elem) {
+    gsap.to(elem, {
+        ease: Expo.easeInOut,
+        width: "100%",
+        duration: 2,
+        stagger: 2
+    }) 
+});
+array.forEach(element => {
+    
+});
