5ab0b65bf52619da499ef431c3c873d093b4bd0a
vrutansh <vrutanshpatel01@gmail.com>
2023-10-28 21:49:43 +0530

    done

--- Fi-Store/script.js
+++ Fi-Store/script.js
@@ -1,12 +1,24 @@
-vv = document.querySelectorAll(".imgcontainer");
-vv.forEach(function(elem) {
-    gsap.to(elem, {
-        ease: Expo.easeInOut,
-        width: "100%",
-        duration: 2,
-        stagger: 2
-    }) 
-});
-array.forEach(element => {
-    
-});
+var tl = gsap.timeline({
+    repeat: -1
+})
+
+tl
+.to(".imgcontainer", {
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration: 2,
+    stagger: 2
+}, 'a') 
+
+.to(".text h1",{
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:0
+}, 'a')
+
+.to(".text h1",{
+    delay:2,
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:"-100%"
+}, 'a')
